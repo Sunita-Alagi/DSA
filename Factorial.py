64d2cd62d2d7bae1c73ae6301020aa5f2bddb844
Sunita Alagi <123356461+Sunita-Alagi@users.noreply.github.com>
2023-05-01 22:34:07 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -7,3 +7,17 @@ if n >0:
     print(fact)
 else:
     raise ValueError('You must enter a non-negative integer')
+#using recusion
+# Factorial of a number using recursion
+
+print('Enter the positive integer to get factorial')
+n=int(input())
+if n < 0:
+    raise ValueError('You must enter a non-negative 
+else:
+    def fact(n):
+        if n == 1:
+            return n
+        else:
+            return n*fact(n-1)
+
