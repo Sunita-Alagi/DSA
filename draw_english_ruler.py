e25d9451a65d7da5bcea4b541a99178dee84c736
Sunita Alagi <sunitaalagi@Sunitas-MacBook-Air.local>
2023-06-29 18:59:32 +0530

    changes

---
+++ draw_english_ruler.py
@@ -0,0 +1,22 @@
+def draw_line(tick_length, tick_label=" "):
+    line = "-" * tick_length
+    if tick_label:
+        line += " " + tick_label
+    print(line)
+
+
+def draw_interval(center_length):
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
+
+
+def draw_ruler(num_inches, major_length):
+    draw_line(major_length, "0")
+    for j in range(1, 1 + num_inches):
+        draw_interval(major_length - 1)
+        draw_line(major_length, str(j))
+
+
+draw_ruler(4, 4)
