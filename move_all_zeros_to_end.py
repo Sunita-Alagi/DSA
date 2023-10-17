f90ef2e51990d6e4b070ca87de823f73ffd1a9e4
sunitaalagi <sunita.alagi@gmail.com>
2023-10-18 00:15:45 +0530

    enclosed in function

--- move_all_zeros_to_end.py
+++ move_all_zeros_to_end.py
@@ -1,8 +1,12 @@
-list = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
-b = len(list)
-j = 0
-for i in range(b):
-	if list[i] != 0:
-		list[j], list[i] = list[i], list[j] 
-		j += 1
-print(list) 
+def move_zero_to_end(arr):
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[j], arr[i] = arr[i], arr[j]
+            j += 1
+    print(arr)
+
+
+arr = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
+
+move_zero_to_end(arr)
