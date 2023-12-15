5d22bdf7cdbbb6a9528ad5e19f27d14b8db422c2
sunitaalagi <sunita.alagi@gmail.com>
2023-12-15 22:38:22 +0530

    reformatted

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,18 +1,16 @@
-#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return
+# the index where it would be if it were inserted in order.
+def search_insert(nums, target):
+    low, high = 0, len(nums) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
 
-class Solution:
-    def search_insert(nums, target):
-        low, high = 0, len(nums) - 1
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1  
 
-        while low <= high:
-            mid = (low + high) // 2
-
-            if nums[mid] == target:
-                return mid  
-            elif nums[mid] < target:
-                low = mid + 1  
-            else:
-                high = mid - 1  
-
-        return low  
+    return low
